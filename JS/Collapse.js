2c30856c1ef154eca0e52f2572a81a3f214c5252
eddiebus <edwinbusulwa@gmail.com>
2022-09-24 20:10:57 +0100

    Collapsible Update
    Collapsible elements now have an Icon to show if open or close. Now a user can tell that they can click on it. no?

--- JS/Collapse.js
+++ JS/Collapse.js
@@ -3,6 +3,8 @@ function CollToggle(event) {
     let content = event.target.nextElementSibling;
     let currentStyle = content.style.display;
     console.log(currentStyle);
+
+    event.target.classList.toggle("active");
     if (content.style.maxHeight){
         content.style.maxHeight = null;
     }
@@ -10,8 +12,6 @@ function CollToggle(event) {
         content.style.maxHeight = content.scrollHeight + "px";
     }
 }
-
-
 
 function CollapseInit() {
     let Coll_Elements = document.getElementsByClassName("Collapsible")
